523f4e45d94b1881e50d32cf99154c7edd8cf9d6
mysterhayde <hdougoud@student.42.fr>
2025-01-15 20:13:46 +0100

    pipex verification

--- Sources/main.c
+++ Sources/main.c
@@ -6,7 +6,46 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:07:38 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/01/14 16:07:39 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/01/15 19:58:18 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+static void check_arguments(int argc, char **argv)
+{
+	if (argc != 5)
+	{
+		if (argc > 5)
+			ft_putendl_fd("Error\nToo mouch arguments", 2);
+		else if (argc < 5)
+			ft_putendl_fd("Error\nNot enough arguments", 2);
+		exit(EXIT_FAILURE);
+	}
+	if (access(argv[1], R_OK) == -1)
+	{
+		perror("Error\nFile 1");
+		exit(EXIT_FAILURE);
+	}
+	if (access(argv[2], X_OK) == -1)
+	{
+		perror("Error\nCmd 1");
+		exit(EXIT_FAILURE);
+	}
+	if (access(argv[3], X_OK) == -1)
+	{
+		perror("Error\nCmd 2");
+		exit(EXIT_FAILURE);
+	}
+	if (access(argv[4], W_OK) == -1)
+	{
+		perror("Error\nFile 2");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	check_arguments(argc, argv);
+	return (0);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 13:27:54 by hdougoud          #+#    #+#             */
-/*   Updated: 2024/12/09 15:42:25 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/01/15 19:10:25 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,15 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
+# include <fcntl.h>
 
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
+
+# define COLOR_RED 			"\x1b[31m"
+# define COLOR_GREEN 		"\x1b[32m"
+# define COLOR_YELLOW		"\x1b[33m"
+# define COLOR_RESET		"\x1b[0m"
 
 //---------------------------------LIBFT----------------------------------------
 
