d01ce4bbed7a742b758349f8c7896c507f03b7c5
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-01-16 23:26:43 +0100

    get binary path

---
+++ Sources/binary.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   binary.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 16:46:36 by hdougoud          #+#    #+#             */
+/*   Updated: 2025/01/16 23:24:56 by hdougoud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static char	*get_path_cmd(char **path, char *argv)
+{
+	int		i;
+	char	*tmp;
+	char	*cmd;
+
+	i = 0;
+	while(path[i])
+	{
+		tmp = ft_strjoin(path[i], "/");
+		cmd = ft_strjoin(tmp, argv);
+		free(tmp);
+		if (access(cmd, F_OK | X_OK) == 0)
+			return (cmd);
+		free(cmd);
+		i++;
+	}
+	return (NULL);
+}
+
+void	get_binary(t_path *path, int argc, char **argv)
+{
+	int		i;
+	char	*cmd;
+
+	i = 0;
+	while (i < argc - 3)
+	{
+		cmd = get_path_cmd(path->path, argv[i + 2]);
+			if (!cmd)
+				show_error("Cannot find binary");
+		path->cmd[i] = cmd;
+		i++;
+	}
+}
+
+char	**get_path(char **envp, char **argv, int argc)
+{
+	int		i;
+	char	*env;
+	char	**path;
+
+	i = 0;
+	while (envp[i])
+	{
+		if ((env = ft_strnstr(envp[i], "PATH=", 5)) != NULL)
+			break;
+		i++;	
+	}
+	if (!env)
+		show_error("Cannot find PATH in env");
+	path = ft_split(env, ':');
+	return (path);
+}
+
--- Sources/main.c
+++ Sources/main.c
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 16:23:44 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/01/16 17:05:14 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/01/16 23:25:23 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,47 @@ void	show_error(char *str)
 	exit(EXIT_FAILURE);
 }
 
-static void	check_args(char **envp, char **argv, int argc)
+
+
+static void	check_args(int argc, char **argv, t_path *path)
 {
-	int	i;
+	int		i;
+	int		j;
+	char 	*tmp;
+	char	*cmd;
 
 	i = 0;
-	if (access(argv[1], R_OK) == -1)
+	j = 0;
+	if (access(argv[1], F_OK | R_OK) == -1)
 		show_error("Cannot open input file");
-	if (access(argv[argc - 1], W_OK) == -1)
-		show_error("Cannot open output_file");
-	while (envp[i])
-	{
+	if (access(argv[argc - 1], F_OK | W_OK) == -1)
+		show_error("Cannot open output file");
+	// while (i < argc - 3)
+	// {
 		
-	}
-	
-	
+	// }
 }
 
 int main(int argc, char **argv, char **envp)
 {
+	t_path	path;
+	
 	if (argc < 4)
 	{
 		ft_putendl_fd("Error not enough args", 2);
 		exit(EXIT_FAILURE);
 	}
-	check_args(envp, argv, argc);
+	path.path = get_path(envp, argv, argc);
+	path.cmd = malloc(sizeof(char *) * (argc - 2));
+	if (!path.cmd)
+		show_error("Malloc failed");
+	get_binary(&path, argc, argv);
+	check_args(argc, argv, &path);
+	printf("%s\n", path.cmd[0]);
+	printf("%s\n", path.cmd[1]);
+	printf("%s\n", path.cmd[2]);
+	printf("%s\n", path.cmd[3]);
+	printf("%s\n", path.cmd[4]);
+	printf("%s\n", path.cmd[5]);
 	return (0);
 }
