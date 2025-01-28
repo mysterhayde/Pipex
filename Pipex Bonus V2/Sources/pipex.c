77954b728ef5d5ef56f5d4c38bd2bf888322a504
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-01-28 17:00:07 +0100

    pipex return status

--- Pipex Bonus V2/Sources/pipex.c
+++ Pipex Bonus V2/Sources/pipex.c
@@ -6,13 +6,13 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:12:23 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/01/27 16:46:57 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/01/28 14:42:04 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	children_patern(t_path *path, int *pipefd)
+static void	children_patern_1(t_path *path, int *pipefd)
 {
 	close(pipefd[0]);
 	if (dup2(pipefd[1], STDOUT_FILENO) == -1)
@@ -25,7 +25,7 @@ static void	children_patern(t_path *path, int *pipefd)
 	return ;
 }
 
-static void	parent_patern(t_path *path, int *pipefd)
+static void	children_partern_2(t_path *path, int *pipefd)
 {
 	close(pipefd[1]);
 	if (dup2(pipefd[0], STDIN_FILENO) == -1)
@@ -33,7 +33,6 @@ static void	parent_patern(t_path *path, int *pipefd)
 	if (dup2(path->fd_2, STDOUT_FILENO) == -1)
 		show_error(path, "Dup2 failed");
 	close(pipefd[0]);
-	waitpid(-1, NULL, 0);
 	if (execve(path->binary[1], path->cmd[1], NULL) == -1)
 		show_error(path, "Execve failed");
 	return ;
@@ -41,16 +40,26 @@ static void	parent_patern(t_path *path, int *pipefd)
 
 void	pipex(t_path *path)
 {
-	pid_t	pid;
+	int		status;
+	pid_t	pid_1;
+	pid_t	pid_2;
 	int		pipefd[2];
 
 	if (pipe(pipefd) == -1)
 		show_error(path, "Pipe failed");
-	pid = fork();
-	if (pid == -1)
+	pid_1 = fork();
+	if (pid_1 == -1)
 		show_error(path, "Fork failed");
-	if (pid == 0)
-		children_patern(path, pipefd);
-	else
-		parent_patern(path, pipefd);
+	if (pid_1 == 0)
+		children_patern_1(path, pipefd);
+	pid_2 = fork();
+	if (pid_2 == -1)
+		show_error(path, "Fork failed");
+	if (pid_2 == 0)
+		children_partern_2(path, pipefd);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(pid_1, &status, 0);
+	waitpid(pid_2, &status, 0);
+	return ;
 }
