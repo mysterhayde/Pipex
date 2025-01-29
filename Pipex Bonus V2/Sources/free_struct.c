e9cfaece1437f4aa3ac604873f901dccf75c2022
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-01-29 19:28:25 +0100

    pipex V2 finished

--- Pipex Bonus V2/Sources/free_struct.c
+++ Pipex Bonus V2/Sources/free_struct.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_struct.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hayden <hayden@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 16:10:39 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/01/27 23:35:57 by hayden           ###   ########.fr       */
+/*   Updated: 2025/01/29 19:17:41 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@ static void	free_tab(char **tab)
 	while (tab[i])
 	{
 		free(tab[i]);
+		tab[i] = NULL;
 		i++;
 	}
 	free(tab);
+	tab = NULL;
 }
 
 void	free_struct(t_path *path)
@@ -34,9 +36,13 @@ void	free_struct(t_path *path)
 		free_tab(path->path);
 	if (path->binary)
 		free_tab(path->binary);
-	while (path->cmd[i])
-		free_tab(path->cmd[i++]);
-	free(path->cmd);
+	if (path->cmd)
+	{
+		while (path->cmd[i])
+			free_tab(path->cmd[i++]);
+		free(path->cmd);
+		path->cmd = NULL;
+	}
 	if (path->fd_1 > 0)
 		close(path->fd_1);
 	if (path->fd_2 > 0)
